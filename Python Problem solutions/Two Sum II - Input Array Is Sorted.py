bca9fcc2df2a2c363fc60f2dc2a6af2afdd67908
Prasanna <prasannagollavilli@gmail.com>
2025-12-13 18:42:41 +0530

    two sum II

---
+++ Python Problem solutions/Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        i=0
+        j=len(numbers)-1
+        while i!=j:
+            s=numbers[i]+numbers[j]
+            if s==target:
+                return [i+1,j+1]
+            elif s<target:
+                i=i+1
+            else:
+                j=j-1
+        
