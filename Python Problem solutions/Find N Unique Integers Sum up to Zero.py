97765e1aa1901bb7b26b3fbc1761ac960939a420
Prasanna <prasannagollavilli@gmail.com>
2026-01-03 01:03:12 +0530

    sum_up to zero

---
+++ Python Problem solutions/Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sumZero(self, n):
+        lis=[]
+        for i in range(1,n//2+1):
+            lis.append(i)
+            lis.append(-i)
+        if n%2!=0:
+            lis.append(0) 
+        return lis
+
+        
