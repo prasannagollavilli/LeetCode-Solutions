736dd0888d2f3dba60ef26195b47ac7923b8d588
Prasanna <prasannagollavilli@gmail.com>
2026-02-24 00:16:07 +0530

    jewels and stones

---
+++ Python Problem solutions/771. Jewels and Stones.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        c=0
+        for i in stones:
+            if i in jewels:
+                c=c+1
+        return c
+        
